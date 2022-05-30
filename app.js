c49e5ef919d5ab9ab667e94ed4c4ced0bfa0000a
Leobardo Pineda <leobapineda@gmail.com>
2022-05-30 17:27:52 -0500

    Feat: modify display of weather value for better UI

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ let weather = {
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".temp").innerText = temp + " °C";
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText =
